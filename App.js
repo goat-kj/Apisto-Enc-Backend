0c3cc005a734b022904262b38415279843eb7006
Kyoungjun Han <kjers96@gmail.com>
2023-03-17 23:18:55 +0900

    backend completed (for now)

--- App.js
+++ App.js
@@ -1,19 +1,21 @@
 const express = require('express');
 const sqlite3 = require('sqlite3').verbose();
+const cors = require('cors');
 
 const app = express();
 const port = 4000;
-const db = new sqlite3.Database('./apistogramma.db');
+const db = new sqlite3.Database('./apisto.db');
 
 db.serialize(() => {
-  db.run('CREATE TABLE IF NOT EXISTS fish (name TEXT, origin TEXT, size REAL, pH REAL, image TEXT)');
+  db.run('CREATE TABLE IF NOT EXISTS apisto (id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT, origin TEXT, size TEXT, pH TEXT, image TEXT)');
 });
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(cors({ origin: 'http://localhost:3000' }));
 
 app.get('/all', (req, res) => {
-    db.all('SELECT * FROM fish', [], (err, rows) => {
+    db.all('SELECT * FROM apisto', [], (err, rows) => {
       if (err) {
         return console.error(err.message);
       }
@@ -21,23 +23,23 @@ app.get('/all', (req, res) => {
     });
   });
 
-app.post('/create', (req,res) => {
+  app.post('/create', (req,res) => {
     const fish = req.body;
-    db.run(`INSERT INTO fish (name, origin, size, pH, image) VALUES (?, ?, ?, ?, ?)`, [fish.name, fish.origin, fish.size, fish.pH, fish.image], function(err) {
-        if (err) {
-          return console.log(err.message);
-        };
-        console.log(`A row has been inserted with rowid ${this.lastID}`);
-    res.status(201).send('Created Sucessfully');
+    db.run(`INSERT INTO apisto (name, origin, size, pH, image) VALUES (?, ?, ?, ?, ?)`, [fish.name, fish.origin, fish.size, fish.pH, fish.image], function(err) {
+      if (err) {
+        return console.log(err.message);
+      };
+      console.log(`A row has been inserted with rowid ${this.lastID}`);
+      res.status(201).send('Created Successfully');
+    });
   });
-});
 
 app.put('/update/:id', (req, res) => {
     const fishId = req.params.id;
     const updatedFish = req.body;
   
     db.run(
-      `UPDATE fish SET name = ?, origin = ?, size = ?, pH = ?, image = ? WHERE rowid = ?`,
+      `UPDATE apisto SET name = ?, origin = ?, size = ?, pH = ?, image = ? WHERE rowid = ?`,
       [updatedFish.name, updatedFish.origin, updatedFish.size, updatedFish.pH, updatedFish.image, fishId],
       function(err) {
         if (err) {
@@ -51,7 +53,7 @@ app.put('/update/:id', (req, res) => {
 
 app.delete('/delete/:id', (req, res) => {
     const id = req.params.id;
-    db.run(`DELETE FROM fish WHERE rowid=?`, id, function(err) {
+    db.run(`DELETE FROM apisto WHERE rowid=?`, id, function(err) {
       if (err) {
         return console.error(err.message);
       }
